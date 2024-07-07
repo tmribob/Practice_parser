df766468383a7e57afd9fdcdfaa39f7b8432098f
Tmribob <mmitko2141@gmail.com>
2024-07-07 20:17:41 +0400

    finish docker-compose

--- backend/config.py
+++ backend/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     def database_url_psycopg(self):
         return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
-    model_config = SettingsConfigDict(env_file='C:\Practice1\.env')
+    model_config = SettingsConfigDict(env_file='.env')
 
 
 settings = Settings()
